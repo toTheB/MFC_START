2df678ff8b274dac6d762e19571a267c0405c048
toTheB <y1394653897@163.com>
2022-10-26 22:15:16 +0800

    添加项目文件。

---
+++ MFC_START/MFC_STARTView.cpp
@@ -0,0 +1,160 @@
+﻿
+// MFC_STARTView.cpp: CMFCSTARTView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC_START.h"
+#endif
+
+#include "MFC_STARTDoc.h"
+#include "MFC_STARTView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCSTARTView
+
+IMPLEMENT_DYNCREATE(CMFCSTARTView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCSTARTView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_MOUSEACTIVATE()
+	ON_WM_MBUTTONDOWN()
+	ON_WM_MBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCSTARTView 构造/析构
+
+CMFCSTARTView::CMFCSTARTView() noexcept
+{
+	// TODO: 在此处添加构造代码
+	_oldPen = nullptr;
+	_mouseDown = false;
+}
+
+CMFCSTARTView::~CMFCSTARTView()
+{
+}
+
+BOOL CMFCSTARTView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCSTARTView 绘图
+
+void CMFCSTARTView::OnDraw(CDC* pDC)
+{
+	CMFCSTARTDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	if (_mouseDown)
+	{
+		CPen solidPen;
+		solidPen.CreatePen(PS_SOLID, 1, AFX_IDC_COLOR_DARKBLUE);
+		// 切换画笔
+		_oldPen = pDC->SelectObject(&solidPen);
+		pDC->SetROP2(R2_XORPEN);
+		pDC->SelectStockObject(NULL_BRUSH);
+		pDC->Rectangle(_startPoint.x, _startPoint.y, _oldPoint.x, _oldPoint.y);
+		pDC->Rectangle(_startPoint.x, _startPoint.y, _curPoint.x, _curPoint.y);
+		_oldPoint = _curPoint;
+		// 恢复画笔
+		pDC->SelectObject(_oldPen);
+	}
+
+}
+
+
+// CMFCSTARTView 打印
+
+BOOL CMFCSTARTView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCSTARTView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFCSTARTView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFCSTARTView 诊断
+
+#ifdef _DEBUG
+void CMFCSTARTView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCSTARTView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCSTARTDoc* CMFCSTARTView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCSTARTDoc)));
+	return (CMFCSTARTDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+
+
+// CMFCSTARTView 消息处理程序
+
+void CMFCSTARTView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	_curPoint = point;
+	Invalidate(0);
+
+	CView::OnMouseMove(nFlags, point);
+}
+
+
+void CMFCSTARTView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	_mouseDown = true;
+	_startPoint = point;
+	_oldPoint = point;
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCSTARTView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	_mouseDown = false;
+
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
---
+++ MFC_START/MFC_STARTView.h
@@ -0,0 +1,74 @@
+﻿
+// MFC_STARTView.h: CMFCSTARTView 类的接口
+//
+
+#pragma once
+
+
+class CMFCSTARTView : public CView
+{
+protected: // 仅从序列化创建
+	CMFCSTARTView() noexcept;
+	DECLARE_DYNCREATE(CMFCSTARTView)
+
+		// 特性
+public:
+	CMFCSTARTDoc* GetDocument() const;
+
+	// 操作
+public:
+
+	// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+	// 实现
+public:
+	virtual ~CMFCSTARTView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+	// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+		// 消息处理
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+
+	// 成员变量
+protected:
+	CPen* _oldPen;
+	CPoint _startPoint;
+	CPoint _oldPoint;
+	CPoint _curPoint;
+	bool _mouseDown;
+
+	//常量
+public:
+	static const int DRAW_DOT = 0;
+	static const int DRAW_LINE = 1;
+	static const int DRAW_REC = 2;
+	static const int DRAW_CIRCLE = 3;
+	static const int DRAW_POLY = 4;
+	static const int DRAW_CURVE = 5;
+};
+
+#ifndef _DEBUG  // MFC_STARTView.cpp 中的调试版本
+inline CMFCSTARTDoc* CMFCSTARTView::GetDocument() const
+{
+	return reinterpret_cast<CMFCSTARTDoc*>(m_pDocument);
+}
+#endif
+
